08db05e45f1e608536534c23a99eb37cad949052
mokone-september <mokoneseptember@gmail.com>
2025-06-04 15:44:32 +0200

    Add new projects: meduim blog with descriptions

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -211,7 +211,7 @@ export function Project() {
                 <a href="https://github.com/mokone-september/Kasi-Pizza" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
-                 <a href="https://kasi-pizza.vercel.app/https:/" target="_blank" rel="noreferrer">
+                 <a href="https://kasi-pizza.vercel.app/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a> 
               </div>
@@ -229,11 +229,137 @@ export function Project() {
                 <li>Tailwind CSS</li>
                 <li>Vite</li>
                 <li>Javascript</li>
+                <li>Pizza API</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a href="https://github.com/mokone-september/snake-game" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+                 <a href="https://snake-game-pi-rose.vercel.app/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a> 
+              </div>
+            </header>
+            <div className="body">
+              <h3>Snake Game</h3> 
+              <p>
+              A classic snake game built with React and Redux, featuring a simple and intuitive interface, and smooth gameplay.A classic snake game built with React and Redux, featuring a simple and intuitive interface, and smooth gameplay.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Redux</li>
+                <li>Typescript</li>
+                <li>TailwindCSS</li>
+                <li>Typescript</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a href="https://github.com/mokone-september/recipe-app" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+                 <a href="https://recipe-app-liard-seven.vercel.app/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a> 
+              </div>
+            </header>
+            <div className="body">
+              <h3>Recipe App</h3>
+              <p>
+              Flavor Fiesta is a vibrant culinary discovery platform that empowers users to explore global cuisines through an intuitive and interactive interface. Powered by the MealDB API, the app showcases an extensive collection of recipes with detailed information about ingredients, preparation steps, and regional cuisines.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Vite</li>
+                <li>TailwindCSS</li>
+                <li>MealDB API</li>
+                <li>Typescript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+          
+         <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a href="https://github.com/mokone-september/ToDo-app" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+                 <a href="https://to-do-app-sand-eight.vercel.app/ " target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a> 
+              </div>
+            </header>
+            <div className="body">
+              <h3>ToDo-App</h3>
+              <p>
+             To-do list website made with React JS, Tailwind CSS, TypeScript, Redux Toolkit.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Redux</li>
+                <li>Tailwind CSS</li>
+                <li>Typescript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+        
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                 <a href="https://medium.com/@mokoneseptember" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a> 
+              </div>
+            </header>
+            <div className="body">
+              <h3>Medium</h3>
+              <p>
+              Medium is a blogging platform that allows users to share their thoughts, stories, and ideas with a global audience. It provides a clean and minimalist interface for writing and reading articles, making it easy for anyone to publish content and engage with readers.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
       </div>
     </Container>
   );
